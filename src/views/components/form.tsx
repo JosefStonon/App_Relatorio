4a88f4d3e5fd95f0b87dd2df9da694c6e7b21373
josefSartori <josef.sartori@outlook.com>
2025-11-04 21:47:29 -0300

    feat: error implementation at input

--- src/views/components/form.tsx
+++ src/views/components/form.tsx
@@ -83,6 +83,11 @@ export function Form() {
                   className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
                 />
               </div>
+              {formState.errors.email && (
+                <small className="text-red-400">
+                  Este campo é obrigatório!
+                </small>
+              )}
             </div>
 
             <div className="sm:col-span-3">
@@ -123,6 +128,11 @@ export function Form() {
                   className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
                 />
               </div>
+              {formState.errors.age && (
+                <small className="text-red-400">
+                  Este campo é obrigatório!
+                </small>
+              )}
             </div>
           </div>
         </div>
