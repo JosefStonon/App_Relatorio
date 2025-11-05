53d62af85e2c3e22ce9a7ae4faf64253cc75d944
josefSartori <josef.sartori@outlook.com>
2025-11-04 21:41:31 -0300

    feat: hook-form implementation

--- src/views/components/form.tsx
+++ src/views/components/form.tsx
@@ -1,6 +1,35 @@
+import { useForm } from 'react-hook-form';
+
+interface IformData {
+  first_name: string;
+  email: string;
+  sexo: string;
+  age: number;
+}
+
 export function Form() {
+  const {
+    handleSubmit: form,
+    register,
+    formState,
+    reset,
+  } = useForm<IformData>({
+    defaultValues: {
+      first_name: '',
+      email: '',
+      sexo: '',
+      age: undefined,
+    },
+  });
+
+  const handleSubmit = form((data) => {
+    console.log('enviou');
+    console.log(data);
+    reset();
+  });
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="space-y-12">
         <div className="border-b border-white/10 pb-12">
           <h2 className="text-base/7 font-semibold text-white">
@@ -13,20 +42,28 @@ export function Form() {
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
               <label
-                htmlFor="first-name"
+                htmlFor="first_name"
                 className="block text-sm/6 font-medium text-white"
               >
                 First name
               </label>
               <div className="mt-2">
                 <input
-                  id="first-name"
-                  name="first-name"
+                  {...register('first_name', {
+                    required: true,
+                  })}
+                  id="first_name"
+                  name="first_name"
                   type="text"
                   autoComplete="given-name"
                   className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
                 />
               </div>
+              {formState.errors.first_name && (
+                <small className="text-red-400">
+                  Este campo é obrigatório!
+                </small>
+              )}
             </div>
 
             <div className="sm:col-span-4">
@@ -38,6 +75,7 @@ export function Form() {
               </label>
               <div className="mt-2">
                 <input
+                  {...register('email')}
                   id="email"
                   name="email"
                   type="email"
@@ -49,13 +87,14 @@ export function Form() {
 
             <div className="sm:col-span-3">
               <label
-                htmlFor="country"
+                htmlFor="sexo"
                 className="block text-sm/6 font-medium text-white"
               >
                 Sexo
               </label>
               <div className="mt-2 grid grid-cols-1">
                 <select
+                  {...register('sexo')}
                   id="sexo"
                   name="sexo"
                   autoComplete="gender-sexo"
@@ -76,6 +115,7 @@ export function Form() {
               </label>
               <div className="mt-2">
                 <input
+                  {...register('age')}
                   id="age"
                   name="age"
                   type="number"
@@ -89,7 +129,7 @@ export function Form() {
       </div>
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button type="button" className="text-sm/6 font-semibold text-white">
+        <button type="reset" className="text-sm/6 font-semibold text-white">
           Cancel
         </button>
         <button
