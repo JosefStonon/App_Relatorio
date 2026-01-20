e32b458fab6054e76d4adea6bc625e7f436b0911
josefSartori <josef.sartori@outlook.com>
2026-01-19 23:17:45 -0300

    feat: suit the project to s3 aws

--- src/Route.tsx
+++ src/Route.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import { Form } from './views/components/form.tsx';
 import { Home } from './views/components/home.tsx';
 import { Machine } from './views/components/machines.tsx';
-import { TablesCompanies } from './views/components/tablesCompanies.tsx';
 export function AppRoutes() {
   return (
     <BrowserRouter>
@@ -10,7 +9,6 @@ export function AppRoutes() {
         <Route index element={<Home />} />
         <Route path="/form" element={<Form />} />
         <Route path="/machines" element={<Machine />} />
-        <Route path="/tablesCompanies" element={<TablesCompanies />} />
       </Routes>
     </BrowserRouter>
   );
--- src/views/components/form.tsx
+++ src/views/components/form.tsx
@@ -7,11 +7,11 @@ import type { Ierrors, IformData } from '../../types';
 export function Form() {
   const {
     handleSubmit: form,
-    register,
+    register, //registra os campos do formulario
     formState,
-    reset,
-    getValues,
-    setValue,
+    reset, //reseta o formulario
+    getValues, //pega o valor do campo
+    setValue, //seta/coloca o valor do campo
   } = useForm<IformData>({});
 
   const { toast } = useToast();
@@ -26,7 +26,6 @@ export function Form() {
       setValue('bairro', data.bairro);
       setValue('cidade', data.localidade);
       setValue('estado', data.uf);
-      console.log('Endereço encontrado:', data);
     } catch (error) {
       console.error('Erro ao buscar o CEP:', error);
     }
